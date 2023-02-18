6b5bc98ba5b9a793dae0ff16b844c6c4a6c1d020
denisdevke <deniskem.dev@gmail.com>
2023-02-18 20:14:06 +0300

    [Finishes #184512556] added the method to get the stk callback

--- test.php
+++ test.php
@@ -3,13 +3,6 @@
 require_once realpath(__DIR__ . '/vendor/autoload.php');
 
 use Denniskemboi\PaymentGateway\Mpesa\StkPush;
-
-$data = new StkPush();
-$data->setCallbackUrl("https://encv2yd32vb2e.x.pipedream.net/") // required
-    ->setAmount("1") // required
-    ->setPhone("254724974848") // required
-    ->setReference("House Rent") //reuired *
-    ->setRemarks("your remarks"); // optional
-echo json_encode($data->tillRequestPush());
+echo json_encode(StkPush::getCallbackData());
 echo "\n";
 ?>
