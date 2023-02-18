ea7975bddb8c589b6f86c36ecdbfdd9f8ee4f355
denisdevke <deniskem.dev@gmail.com>
2023-02-18 10:56:40 +0300

    [Finishes #184511007] Added the feature for getting live token

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once realpath(__DIR__ . '/vendor/autoload.php');
+
+use Denniskemboi\PaymentGateway\Mpesa\App;
+
+$app = new App();
+echo json_encode($app->getLiveToken());
+echo "\n";
+
+?>
